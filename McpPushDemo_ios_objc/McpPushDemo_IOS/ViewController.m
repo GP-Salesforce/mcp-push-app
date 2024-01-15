e29a33a79ebc95cf6b0f300a445ce689d0e0c6a7
nhkim81 <nhkim@goldenplanet.co.kr>
2024-01-15 15:25:03 +0900

    [ios][objc] 프로젝트 재생성

---
+++ McpPushDemo_ios_objc/McpPushDemo_IOS/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  McpPushDemo_IOS
+//
+//  Created by 김남훈 on 1/15/24.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
